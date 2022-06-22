3abe3841ee98db4f6dcaa18e5ae48b2dae68e6ca
michaelHoffman <mphoffman8390@gmail.com>
2022-06-22 16:30:02 -0400

    outline

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
-
-// state
-let initialState;
-
-function buildInitialState() {
-    const gameState = {
-        players: ['X', 'O'],
-        board: [
-            [null, null, null],
-            [null, null, null],
-            [null, null, null] 
-        ]
-    }
-
-}
-
-// render
-function renderState() {
-
-}
-
-// maybe a dozen or so helper functions for tiny pieces of the interface
-
-// listeners
-function onBoardClick(event) {
-  console.log(event.target.innerText)
-  event.target.innerHTML = 'O'
-
-
+const gameBoard = {
+    players: ['X', 'O'],
+    board: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null] 
+    ], 
+    currentPlayer: 0
 }
 const board = document.getElementById('gameBoard');
 console.log(board);
+console.log(gameBoard)
 
 board.addEventListener("click", onBoardClick)
+
+function onBoardClick(event) {
+   updateState()
+   updateHTML()
+  let win = checkWin()
+  if(win){
+      //do win thing 
+  } else {changePlayer()}
+  
+}
+function checkWin(){
+    return 
+}
+function updateHTML(){
+    //update DOM based on gameBoard.board
+}
+function updateState(){
+    //change gameboard.board
+}
+function changePlayer(){
+    if(gameBoard.currentPlayer === 0){
+        gameBoard.currentPlayer = 1
+    } else {
+        gameBoard.currentPlayer = 0
+    }
+}
+// console.log(event.target.innerText)
+ //event.target.innerHTML = 'O'
