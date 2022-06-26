457e45eece8ee18fb5c6660894a1404c152925f6
michaelHoffman <mphoffman8390@gmail.com>
2022-06-26 19:08:02 -0400

    switch players

--- app.js
+++ app.js
@@ -1,42 +1,75 @@
 const gameBoard = {
-    players: ['X', 'O'],
+    player1: ['X'],
+    player2: ['O'],
     board: [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null] 
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0] 
     ], 
-    currentPlayer: 0
+    
 }
+const winCombo = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [6, 4, 2],
+    [2, 5, 8],
+    [1, 4, 7],
+    [0, 3, 6],
+]
+const player1 = 'X';
+const player2 = "O";
+let currentPlayer = player1;
+
 const board = document.getElementById('gameBoard');
+
 console.log(board);
 console.log(gameBoard)
 
+
 board.addEventListener("click", onBoardClick)
 
-function onBoardClick(event) {
-   updateState()
-   updateHTML()
-  let win = checkWin()
-  if(win){
-      //do win thing 
-  } else {changePlayer()}
+
+
+function onBoardClick(event) {  
+const id = event.target.id;
+    if(currentPlayer === player1){
+        event.target.innerHTML = 'X';
+        currentPlayer = player2;
+    } else if(currentPlayer === player2){
+        event.target.innerHTML = 'O';
+        currentPlayer = player1;
+    }
+
+ console.log(id)
+// //    updateState()
+// //    updateHTML()
+// //   let win = checkWin()
+// //   if(win){
+// //       //do win thing 
+// //   } else {changePlayer()}
   
 }
-function checkWin(){
-    return 
+ function boardCheck(){
+//      for(i = 0; i < board.length; i ++)
+//      if(board[i] !== ' '){
+//          onBoardClick()
+//      }
+}
+function checkWin(board){
+    // for (let i = 0; i < board.length; i++);
+    // let letters = Object.values(board);
+    //     if(letters === 'X'){
+    //         return true
+    //     }
+    // return false
+   
 }
 function updateHTML(){
     //update DOM based on gameBoard.board
 }
 function updateState(){
-    //change gameboard.board
-}
-function changePlayer(){
-    if(gameBoard.currentPlayer === 0){
-        gameBoard.currentPlayer = 1
-    } else {
-        gameBoard.currentPlayer = 0
-    }
+    //change gameBoard.board
 }
 // console.log(event.target.innerText)
- //event.target.innerHTML = 'O'
